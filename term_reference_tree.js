c7722f410781fe04905244fc65ff9fe97ba0e885
Bart Kelsey <elbarto@gmail.com>
2011-03-11 15:04:24 -0700

    radios working

--- term_reference_tree.js
+++ term_reference_tree.js
@@ -4,5 +4,24 @@
       $(this).toggleClass('term-reference-tree-collapsed');
       $(this).siblings('ul').slideToggle();
     });
+     
+    $('.term-reference-tree').each(function() {
+      var tree = $(this);
+      checkMaxChoices(tree);
+      $(this).find('input[type=checkbox]').change(function() {
+        checkMaxChoices(tree);
+      });
+    });
   });
+  
+  function checkMaxChoices(item) {
+    var maxChoices = item.attr('data-max-choices');
+    var count = item.find(':checked').length;
+    
+    if(count >= maxChoices) {
+      item.find('input[type=checkbox]:not(:checked)').attr('disabled', 'disabled').parent().addClass('disabled');
+    } else {
+      item.find('input[type=checkbox]').removeAttr('disabled').parent().removeClass('disabled');
+    }
+  }
 })(jQuery);
